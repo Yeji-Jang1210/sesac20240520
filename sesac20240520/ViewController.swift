45cf93608fe2da121dc3e1fa8d2d8fa614de4543
Yeji-Jang1210 <jyg8727@gmail.com>
2024-05-20 09:43:25 +0900

    Initial Commit

---
+++ sesac20240520/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sesac20240520
+//
+//  Created by 장예지 on 5/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
